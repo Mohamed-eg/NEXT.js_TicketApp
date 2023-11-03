71c60ea3c21c212e31225471178a4927ad334aab
Mohamed-eg <83299842+Mohamed-eg@users.noreply.github.com>
2023-11-03 07:17:04 +0200

    updateProcess.env

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -2,7 +2,7 @@ import EditTicketForm from "@/app/(components)/EditTicketForm";
 
 const getTicketById = async (id) => {
   try {
-    const res = await fetch(`${MY_HOST_URL}api/Tickets/${id}`, {
+    const res = await fetch(`${process.env.MY_HOST_URL}api/Tickets/${id}`, {
       cache: "no-store",
     });
 
